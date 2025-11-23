df5d7f4dbf0bfb621cc09c0203ae0b1c534200a1
aasimmaslam <asimaslambohio@gmail.com>
2025-11-23 21:37:16 +0500

    fixed build issues

--- src/components/DevzzHero.tsx
+++ src/components/DevzzHero.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import heroBg from "./assets/bg.png";
 import logoImage from "./assets/logo.png";
 
--- src/components/WorkProcess/WorkProcess.tsx
+++ src/components/WorkProcess/WorkProcess.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Picture5 from "../../images/Picture5.png";
 import Picture6 from "../../images/Picture6.png";
